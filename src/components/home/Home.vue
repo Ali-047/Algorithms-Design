a41615cb6e4a1bb096ddeb859af63e1719ab24d3
pedram <pmam18362@gmail.com>
2025-03-25 17:35:12 +0330

    create project and install dependencies.

---
+++ src/components/home/Home.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import {onMounted} from "vue";
+import HeaderComponents from "./components/HeaderComponents.vue";
+import FooterComponents from "./components/FooterComponents.vue";
+
+onMounted(() => {
+  console.log("in home")
+})
+</script>
+
+<template>
+  <header-components/>
+  <div style="text-decoration-color: crimson;font-size: 200px" class="" >hi home</div>
+  <footer-components/>
+</template>
+
+<style scoped lang="scss">
+
+</style>
