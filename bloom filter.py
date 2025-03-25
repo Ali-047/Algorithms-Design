f1d589ec7305face166e415674bae3580e131d7e
Amirreza ghorbani <amirrezagh23@yahoo.com>
2025-03-26 02:09:39 +0330

    Update bloom filter.py
    add class BloomFilter

--- bloom filter.py
+++ bloom filter.py
@@ -1,2 +1,17 @@
 #bloom filter alghorithm 
 from hashlib import md5
+class BloomFilter:
+    def __init__(self,size ,hash_count):
+        self.size = size 
+        self.hash_count = hash_count
+        self.bit_array = [0] * size 
+        
+        def _hashes (Self , word):
+            hashes=[]
+            
+        
+        
+        
+
+        
+        
