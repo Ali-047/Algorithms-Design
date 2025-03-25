a3c813022908e5f94edd2ceb618c75b440bccfd8
Amirreza ghorbani <amirrezagh23@yahoo.com>
2025-03-26 01:35:33 +0330

    Update levenshtein-alghorithm.py
    add comment

--- levenshtein-alghorithm.py
+++ levenshtein-alghorithm.py
@@ -2,11 +2,13 @@ def levenshtein_distance(s1, s2):
     m, n = len(s1), len(s2)
     dp = [[0] * (n + 1) for _ in range(m + 1)]
 
+    # input 
     for i in range(m + 1):
         dp[i][0] = i
     for j in range(n + 1):
         dp[0][j] = j
 
+    # محاسبه فاصله لِوِنِشتاین
     for i in range(1, m + 1):
         for j in range(1, n + 1):
             if s1[i - 1] == s2[j - 1]:
