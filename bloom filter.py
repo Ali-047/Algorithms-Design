40facd80d68ea9f0ea689f8795181bf382d78f17
Amirreza ghorbani <amirrezagh23@yahoo.com>
2025-03-27 01:41:54 +0330

    Update bloom filter.py
    add 2 lib to them hashlib & seed (they couldn't add 2 same string)

--- bloom filter.py
+++ bloom filter.py
@@ -1,15 +1,18 @@
 #bloom filter alghorithm 
 from hashlib import md5
+import hashlib
+from random import seed
+
+
 class BloomFilter:
     def __init__(self,size ,hash_count):
         self.size = size 
         self.hash_count = hash_count
         self.bit_array = [0] * size 
         
-        def _hashes (Self , word):
-            hashes=[]
-            
-        
+        def _hashe (Self , item):
+            hash_obj = hashlib.md5((item + str(seed)).encode('utf-8'))
+                  
         
         
 
