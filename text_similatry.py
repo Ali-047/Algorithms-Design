3c754ed56afd75cbd9143081fc8c368dac388be4
Amirreza ghorbani <amirrezagh23@yahoo.com>
2025-04-21 23:17:49 +0330

    Update text_similatry.py
    add the def add

--- text_similatry.py
+++ text_similatry.py
@@ -1,18 +1,12 @@
 import tkinter as tk
 import hashlib
 import difflib
-
-# Bloom filter implementation
-
 # Bloom Filter implementation
 class BloomFilter:
-    def __init__(self , size , hash_count):
     def __init__(self, size, hash_count):
         self.size = size
         self.hash_count = hash_count
         self.bit_array = [0] * size
-        
-        def _hash (self , item , seed)
 
     def _hash(self, item, seed):
         hash_obj = hashlib.md5((item + str(seed)).encode('utf-8'))
@@ -21,6 +15,10 @@ class BloomFilter:
     
     def add (self, item ):
         for seed in range (self.hash_count):
+            self.bit_array[self._hash(item , seed)] = 1
+            
+            
+    
         
         
          
