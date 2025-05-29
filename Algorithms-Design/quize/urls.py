714c9c11c94d809536fe1ab4064a892849d22c68
yousef <m.yousef.r84@gmail.com>
2025-05-29 11:59:00 +0330

    config urls

---
+++ Algorithms-Design/quize/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from .views import *
+
+
+urlpatterns = [
+    path('register/',RegisterAPIView.as_view(),name = 'sign up'),
+    path('start/questionnaire/',StartQuestionnaireView.as_view(),name = 'StartQuestionnaire'),
+    path('submit/answer/',SubmitAnswerView.as_view(),name = 'submitAnswer'),
+]
