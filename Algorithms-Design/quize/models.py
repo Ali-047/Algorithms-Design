54a33d8a60f751ada53ffcd05f97f9f156775f6e
yousef <m.yousef.r84@gmail.com>
2025-05-29 10:56:28 +0330

    build models of database of project

---
+++ Algorithms-Design/quize/models.py
@@ -0,0 +1,56 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+# Create your models here.
+class CustomUser(AbstractUser):
+    email = models.CharField(max_length=150, blank=True, null=True)
+    name = models.CharField(max_length=100, blank=True, null=True)
+    username = models.CharField(max_length=11, blank=True, null=True, unique=True)
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='custom_user_groups',
+        blank=True,
+        help_text='The groups this user belongs to.',
+        verbose_name='groups',
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='custom_user_permissions',
+        blank=True,
+        help_text='Specific permissions for this user.',
+        verbose_name='user permissions',
+    )
+
+    def __str__(self):
+        return self.username
+
+
+
+
+class Question(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    text = models.TextField()
+    num_of_question = models.IntegerField()
+    all_questions = models.IntegerField()
+
+
+    def __str__(self):
+        return self.name
+
+class Answer(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='answers')
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answered_at = models.DateTimeField(auto_now_add=True)
+    text_answer = models.TextField(null=True, blank=True)
+
+    class Meta:
+        unique_together = ('questionnaire', 'question')
+
+
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.question
