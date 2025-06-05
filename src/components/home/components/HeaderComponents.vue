160e797179e8cadc0a20423b97b4502bcab69ffe
Ali-047 <nematiali06@gmail.com>
2025-06-05 13:26:37 +0330

    cursor pointer added to header

--- src/components/home/components/HeaderComponents.vue
+++ src/components/home/components/HeaderComponents.vue
@@ -3,24 +3,24 @@
     <div id="header" class="flex h-22 justify-between px-5 text-white items-center" :class="handelScroll">
       <img src="@/assets/logo.svg">
       <div class="flex justify-between w-[500px]  text-lg items-center">
-        <div @click="console.log(isOnTop)" class="flex gap-2">
+        <div @click="console.log(isOnTop)" class="flex gap-2 cursor-pointer">
           <img src="@/assets/icon/home.svg"/>
           <div>Home</div>
         </div>
-        <div class="flex gap-2">
+        <div class="flex gap-2 cursor-pointer">
           <img src="@/assets/icon/abut_us.svg"/>
           <div>About Us</div>
         </div>
-        <div class="flex gap-2">
+        <div class="flex gap-2 cursor-pointer">
           <img src="@/assets/icon/call.svg"/>
           <div>Contact Us</div>
         </div>
-        <div class="flex gap-2">
+        <div class="flex gap-2 cursor-pointer">
           <img src="@/assets/icon/note.svg"/>
           <div>Start Exam</div>
         </div>
       </div>
-      <div @click="$router.push({name:'auth',params:{type:'login'}})" class="me-10 flex gap-2">
+      <div @click="$router.push({name:'auth',params:{type:'login'}})" class="me-10 flex gap-2 cursor-pointer">
         <img src="@/assets/icon/login.svg"/>
         <div>Log in</div>
       </div>
