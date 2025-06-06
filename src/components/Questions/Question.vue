3f9424414b7c347169f6303e82ae2fddb8c0142d
Ali-047 <nematiali06@gmail.com>
2025-06-07 00:14:35 +0330

    timer created & added

--- src/components/Questions/Question.vue
+++ src/components/Questions/Question.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
-
+import Timer from "@/components/Questions/Timer.vue";
 </script>
 
 <template>
   <div class="background flex  justify-around items-center  w-[100vw] h-[100vh]">
     <div class="backgroundShadow w-3/4 h-[75vh] bg-gray opacity-70 rounded-4xl box-border px-10 pt-10">
-      <p class="text-white">Question form question one</p>
+      <p class="text-white flex justify-between items-center">Question form question one <Timer/></p>
       <textarea
+          placeholder="type your answer here"
           class="w-full h-[70%] mt-5 text-white border-2 border-white rounded-2xl box-border px-5 pt-5 resize-none bg-gray-dark"></textarea>
       <div class="w-full h-10 flex items-center justify-center mt-5">
         <button class="bg-primary px-5 py-2 cursor-pointer rounded-lg button text-white" @click="$router.push('/Question')">
---
+++ src/components/Questions/Timer.vue
@@ -0,0 +1,37 @@
+<script setup>
+import {ref, onMounted} from "vue";
+
+let minute = ref(0);
+let second = ref(0);
+
+function startTimer() {
+  if (second.value === 59) {
+    minute.value++;
+    second.value = 0;
+  }
+  second.value++;
+}
+
+const interval = setInterval(() => {
+  startTimer();
+} , 1000)
+
+onMounted(() => {
+  startTimer();
+})
+</script>
+
+<template>
+  <div class="text-white">
+    <p>
+      <span v-if="minute >= 0 && minute < 10">0</span>
+      {{ minute }} :
+      <span v-if="second >= 0 && second < 10">0</span>
+      {{ second }}
+    </p>
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
